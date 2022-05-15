2b83b0e3176f76a95bd1ea134ca4703e81b04369
alvesmwanakunda <thecal4@gmail.com>
2022-05-15 20:37:54 +0000

    Api listAvoir model

---
+++ api/models/listAvoir.model.js
@@ -0,0 +1,41 @@
+(function(){
+
+    "use strict";
+    var mongoose = require("mongoose");
+    var Schema = mongoose.Schema;
+
+    var listAvoirSchema = new Schema({
+
+        entreprise:{
+            type: Schema.ObjectId,
+            ref: "Entreprises",
+            required: true
+        },
+        client:{
+            type: Schema.ObjectId,
+            ref: "Clients",
+            required: true
+        },
+        montant:{
+            type:Number,
+            required:false
+        },
+        creation:{
+            type:Date,
+            required:false
+        },
+        type:{
+            type:String,
+            required:false
+        },
+        idRef:{
+            type: Schema.ObjectId,
+            required: false
+        }
+    });
+
+    module.exports={
+        ListAvoirSchema : listAvoirSchema,
+        ListAvoirModel : mongoose.model('ListAvoir',listAvoirSchema)
+    }
+})();
